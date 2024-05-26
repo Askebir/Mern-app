b59572b31cba6279c9c53c836a04c568c15d6ab2
Askebir <askebirtadesse@gmail.com>
2024-05-26 02:36:28 -0700

    final add

---
+++ frontend/src/components/GoalItem.jsx
@@ -0,0 +1,16 @@
+import { useDispatch } from "react-redux"
+import {deleteGoal} from '../features/goals/goalSlice'
+function Goalitem() {
+    const dispatch = useDispatch()
+  return (
+    <div className='goal' >
+        <div>
+            {new Date(goal.createdAt).toLocaleString('en-US')}
+        </div>
+        <h2>{goal.text}</h2>
+        <button onClick={()=> dispatchEvent(deleteGoal(goal._id))} className="close">X</button>
+    </div>
+  )
+}
+
+export default Goalitem
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,60 @@
 
+import {useEffect} from 'react'
+import {useNavigage} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/Goalitem'
+import Spinner from '../components/Spinner'
+import {getGoals, reset}from '../features/goals/goals/goalService'
+
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const navigage = useNavigage()
+  const dispatch = useDispatch()
+  
+
+  const {user} = useSelector((state) => state.auth)
+  const {goals, isLoading, isError, message} = useSelector((state)=>state.goals)
+
+  useEffect(()=> {
+    if(isError){
+      console.log(message)
+    }
+
+
+    if(!user){
+      navigage('/login')
+    }
+    dispatch(getGoals())
+
+    return()=>{
+      dispatch(reset())
+    }
+
+
+  },[user, navigage])
+
+  if(isLoading){
+    return <Spinner/>
+  }
+
+  return <>
+          <section className="heading">
+            <h1>Welcom{user && user.name}</h1>
+            <p>Goals Dashboard</p>
+          </section>
+          <GoalForm/>
+          <section className="content">
+            {goals.length>0 ? (<div className='goals' >
+                {goals.map((goal) =>(
+                  <GoalItem key={goal._id} goal={goal} />
+                ))}
+
+            </div>):(<h3> You have not set any gaols</h3>)}
+          </section>
+  
+  
+       </>
+ 
 }
 
 export default Dashboard
